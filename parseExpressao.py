0b84cec0c4f2d55882abea224acc422c6634640d
fapinter <fpinterich@hotmail.com>
2026-03-17 16:18:57 -0300

    Ajuste na tonezinacao das expressoes

--- parseExpressao.py
+++ parseExpressao.py
@@ -23,114 +23,175 @@ def is_float(token: str) -> bool:
     except ValueError:
         return False
 
-#Definição das Transições
-def define_next_state(next_token: str, next_position: int, size: int) -> str:
-    #Evita erros de index out of range pela natureza da captura do próximo estado
-    if next_position >= size:
-        return ''
-    operacoes = {'+','-','*','/', '//', '%', '^'}
-    parenteses = {'(', ')'}
-    if next_token in operacoes:
-        return 'operacao'
-    elif next_token in parenteses:
-        return 'parenteses'
-    elif is_float(next_token):
-        return 'numero'
-    elif next_token.isupper():
-        return 'comando'
-    else:
-        raise InvalidParsingError(f'Caracter [{next_token}] invalido/Mal Formatado')
-
-
 def parseExpressao(linha: str, numero_linha: int) -> Tuple[bool, List[str]]:
-    #Utilizado para Números inteiros e reais
-    def estadoNumero(tokens: List[str], position: int) -> str:
+
+    #Cria tokens de Números inteiros e reais
+    def estadoNumero(linha: str, position: int) -> Tuple[str, int]:
+        idx = position+1
+        token = linha[position]
+        process = True
+        is_float = False
         try:
-            return define_next_state(next_token=tokens[position+1], next_position=position+1, size=len(tokens))
+            while process:
+                if idx >= len(linha):
+                    break
+                char = linha[idx]
+                if char.isdigit():
+                    token += char
+                elif char=='.':
+                    if not is_float:
+                        token+=char
+                        is_float = True
+                    else:
+                        raise InvalidParsingError(f'Número Real [{token+char}] mal formatado')
+                elif char==' ':
+                    process = False
+                else:
+                    raise InvalidParsingError(f'Número [{token+char}] mal formatado')
+                idx += 1
+            return token, idx
         except InvalidParsingError:
             raise
 
-    #Utilizado para Operadores(+, - , *, /, //, %, ^)
-    def estadoOperacao(tokens: List[str], position: int) -> str:
-        try:
-            return define_next_state(next_token=tokens[position+1], next_position=position+1, size=len(tokens))
+    #Cria tokens de Operadores(+, - , *, /, //, %, ^)
+    def estadoOperador(linha: str, position: int) -> Tuple[str, int]:
+        idx = position+1
+        token = linha[position]
+        try:            
+            if idx >= len(linha):
+                return token, idx
+            next_char = linha[idx]
+            if next_char=='/':
+                token+=next_char
+                return token, idx+1
+            elif next_char==' ':
+                return token, idx
+            else:
+                raise InvalidParsingError(f'Operador [{token+next_char}] inválido')
         except InvalidParsingError:
             raise
     
-    #Utilizado para Comandos (MEM, RES)
-    def estadoComando(tokens: List[str], position: int) -> str:
+    #Cria tokens de Comandos (MEM, RES)
+    def estadoComando(linha: str, position: int) -> Tuple[str, int]:
+        idx = position+1
+        token = linha[position]
+        process = True
         try:
-            return define_next_state(next_token=tokens[position+1], next_position=position+1, size=len(tokens))
+            while process:
+                if idx >= len(linha):
+                    break
+                char = linha[idx]
+                if char.isupper():
+                    token += char
+                elif char == ' ':
+                    process = False
+                else:
+                    raise InvalidParsingError(f'Caracter [{token+char}] Inválido/Mal Formatado')
+                idx += 1
+            return token, idx
+
         except InvalidParsingError:
             raise
         
-    #Utilizado para Parênteses
-    def estadoParenteses(stack_parenteses: deque, tokens: List[str], position: int) -> str:
+    #Cria tokens de Parênteses
+    def estadoParenteses(stack_parenteses: deque, linha: str, position: int) -> Tuple[str, int]:
+        idx = position
+        token = linha[idx]
         try:
-            #Fechamentos de parênteses removem o último item do stack
-            if tokens[position] == ')':
-                #Nenhuma abertura no stack
-                if len(stack_parenteses) == 0:
-                    raise InvalidParsingError('Fechamento de parenteses sem abertura')
-                #Remove a ultima abertura de parenteses
-                stack_parenteses.pop()
-            #Abertura de parenteses adicionam um item no final do stack
-            elif tokens[position] == '(':
+            if token == ')':
+                if len(stack_parenteses) > 0:
+                    stack_parenteses.pop()
+                else:
+                    raise InvalidParsingError('Fechamento de parênteses sem abertura')
+            elif token == '(':
                 stack_parenteses.append('(')
-            
-            return define_next_state(next_token=tokens[position+1], next_position=position+1, size=len(tokens))
-        
+            return token, idx+1        
         except InvalidParsingError:
             raise
+    
+    #Definição das Transições
+
+    def define_transition(linha: str, position: int) -> str:
+        operacoes = {'+','-','*','/', '%', '^'}
+        parenteses = {'(', ')'}
+        char = linha[position]
+        if char=='-':
+            # - pode ser interpretado como um sinal de um
+            # número ou o operador da subtração
+            if position+1 >= len(linha):
+                return 'operacao'
+            elif linha[position+1].isdigit():
+                return 'numero'
+            else:
+                return 'operacao'
+        if char in operacoes:
+            return 'operacao'
+        elif char in parenteses:
+            return 'parenteses'
+        elif char.isdigit():
+            return 'numero'
+        elif char==' ':
+            return 'espaco'
+        elif char.isupper():
+            return 'comando'
+        else:
+            raise InvalidParsingError(f'Caracter [{char}] inválido/Mal Formatado')
+
            
     #Stack para controlar abertura e fechamento de parenteses
+    tokens = []
     stack_parenteses = deque()
-    tokens = linha.split(' ')
-    try:
-        next_state = define_next_state(tokens=tokens, next_position=0, size=len(tokens))
-    except InvalidParsingError as ipe:
-        print(f'Linha [{numero_linha}] é inválida: {ipe}')
-        return False, tokens
-
-    for position in range(0, len(tokens)):
+    idx = 0
+    while idx < len(linha):
         try:
+            next_state = define_transition(linha=linha, position=idx)
             match(next_state):
                 case 'numero':
-                    next_state = estadoNumero(tokens=tokens, position=position)
+                    token, idx = estadoNumero(linha=linha, position=idx)
+                    tokens.append(token)
                 case 'parenteses':
-                    next_state = estadoParenteses(stack_parenteses=stack_parenteses, tokens=tokens, position=position)
+                    token, idx = estadoParenteses(stack_parenteses=stack_parenteses, linha=linha, position=idx)
+                    tokens.append(token)
                 case 'operacao':
-                    next_state = estadoOperacao(tokens=tokens, position=position)
+                    token, idx = estadoOperador(linha=linha, position=idx)
+                    tokens.append(token)
                 case 'comando':
-                    next_state = estadoComando(tokens=tokens, position=position)
-                
+                    token, idx = estadoComando(linha=linha, position=idx)
+                    tokens.append(token)
+                #Espaços demarcam a separação de tokens
+                case 'espaco':
+                    idx += 1
+
         except InvalidParsingError as ipe:
             print(f'Linha [{numero_linha}] é inválida: {ipe}')
-            return False, tokens
+            return False, []
     
     #Caso os parênteses estejam desbalanceados, a expressão é considerada inválida
     if len(stack_parenteses) > 0:
         print(f'Linha {numero_linha} eh invalida: Parenteses nao fechados')
-        return False, tokens
+        return False, []
     return True, tokens
         
     
 if __name__ == "__main__":
     test_cases = [
         '5.5.5 2 +',
-        '10,5 4 -',
-        '10 5 $',
+        '-10,5 4 -',
+        '10 ( ) 5 $',
         '10 ( 2 4 +',
-        '10 asa +',
-        '10 12 +',
-        '10 13.5 //'
+        '10 AsA +',
+        '10 +12 +',
+        '10 13.5 //',
+        '10 MEMORIA',
+        '10 RES',
+        '( 10 MEM ) -20 +',
+        '-20 10 +'
     ]
     f_res = open('resultados_teste_parse.txt', 'w')
     for test_idx in range(0, len(test_cases)):
         line = test_cases[test_idx]
-        print(f'[{test_idx}]Tokenized line: {line.split(' ')}')
-
         valid_line, tokenized_string = parseExpressao(linha=line, numero_linha=test_idx)
-        
-        f_res.write(f'Parse[{valid_line}] {line}\n')
+        print(f'[{test_idx}]Tokenized line: {tokenized_string}')
+
+        f_res.write(f'Parse[{valid_line}]\nToken split: {line.split(' ')} : {tokenized_string}\n')
     f_res.close()
