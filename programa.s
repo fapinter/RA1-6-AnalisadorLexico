9cc111dcb1929ff7da8e76f90aa23ebe13519302
Phillip Wan Tcha Yan <phioe2003@gmail.com>
2026-03-25 20:06:04 -0300

    fix parse expressao

--- programa.s
+++ programa.s
@@ -1,13 +1,18 @@
 .data
 label_0_0: .double 0.0
 label_1_0: .double 1.0
+label_3: .double 3
+label_4: .double 4
+label_10: .double 10
+label_2: .double 2
+label_2_5: .double 2.5
+label_5: .double 5
+label_8: .double 8
 label_5_5: .double 5.5
 label_1: .double 1
-label_2: .double 2
-label_3: .double 3
 label_MEM: .double 0.0
 current_line: .word 0
-results: .space 16
+results: .space 80
 stack_base: .space 4096
 stack_top: .word 0
 
@@ -20,6 +25,143 @@ str r0, [r1]
 mov r0, #1
 ldr r1, =current_line
 str r0, [r1]
+ldr r0, =label_3
+vldr d0, [r0]
+bl push_d0
+ldr r0, =label_4
+vldr d0, [r0]
+bl push_d0
+bl pop_to_d0
+bl pop_to_d1
+vadd.f64 d0, d1, d0
+bl push_d0
+bl pop_to_d0
+ldr r0, =results
+add r0, r0, #0
+vstr d0, [r0]
+
+mov r0, #2
+ldr r1, =current_line
+str r0, [r1]
+ldr r0, =label_10
+vldr d0, [r0]
+bl push_d0
+ldr r0, =label_2
+vldr d0, [r0]
+bl push_d0
+bl pop_to_d0
+bl pop_to_d1
+vsub.f64 d0, d1, d0
+bl push_d0
+bl pop_to_d0
+ldr r0, =results
+add r0, r0, #8
+vstr d0, [r0]
+
+mov r0, #3
+ldr r1, =current_line
+str r0, [r1]
+ldr r0, =label_2_5
+vldr d0, [r0]
+bl push_d0
+ldr r0, =label_5
+vldr d0, [r0]
+bl push_d0
+bl pop_to_d0
+bl pop_to_d1
+vmul.f64 d0, d1, d0
+bl push_d0
+bl pop_to_d0
+ldr r0, =results
+add r0, r0, #16
+vstr d0, [r0]
+
+mov r0, #4
+ldr r1, =current_line
+str r0, [r1]
+ldr r0, =label_8
+vldr d0, [r0]
+bl push_d0
+ldr r0, =label_2
+vldr d0, [r0]
+bl push_d0
+bl pop_to_d0
+bl pop_to_d1
+vdiv.f64 d0, d1, d0
+bl push_d0
+bl pop_to_d0
+ldr r0, =results
+add r0, r0, #24
+vstr d0, [r0]
+
+mov r0, #5
+ldr r1, =current_line
+str r0, [r1]
+ldr r0, =label_10
+vldr d0, [r0]
+bl push_d0
+ldr r0, =label_3
+vldr d0, [r0]
+bl push_d0
+bl pop_to_d0
+bl pop_to_d1
+bl op_int_div
+bl push_d0
+bl pop_to_d0
+ldr r0, =results
+add r0, r0, #32
+vstr d0, [r0]
+
+mov r0, #6
+ldr r1, =current_line
+str r0, [r1]
+ldr r0, =label_10
+vldr d0, [r0]
+bl push_d0
+ldr r0, =label_3
+vldr d0, [r0]
+bl push_d0
+bl pop_to_d0
+bl pop_to_d1
+bl op_int_mod
+bl push_d0
+bl pop_to_d0
+ldr r0, =results
+add r0, r0, #40
+vstr d0, [r0]
+
+mov r0, #7
+ldr r1, =current_line
+str r0, [r1]
+ldr r0, =label_2
+vldr d0, [r0]
+bl push_d0
+ldr r0, =label_3
+vldr d0, [r0]
+bl push_d0
+bl pop_to_d0
+bl pop_to_d1
+bl op_pow
+bl push_d0
+bl pop_to_d0
+ldr r0, =results
+add r0, r0, #48
+vstr d0, [r0]
+
+mov r0, #8
+ldr r1, =current_line
+str r0, [r1]
+ldr r0, =label_MEM
+vldr d0, [r0]
+bl push_d0
+bl pop_to_d0
+ldr r0, =results
+add r0, r0, #56
+vstr d0, [r0]
+
+mov r0, #9
+ldr r1, =current_line
+str r0, [r1]
 ldr r0, =label_5_5
 vldr d0, [r0]
 bl push_d0
@@ -29,10 +171,10 @@ vstr d0, [r0]
 bl push_d0
 bl pop_to_d0
 ldr r0, =results
-add r0, r0, #0
+add r0, r0, #64
 vstr d0, [r0]
 
-mov r0, #2
+mov r0, #10
 ldr r1, =current_line
 str r0, [r1]
 ldr r0, =label_1
@@ -57,7 +199,7 @@ vmul.f64 d0, d1, d0
 bl push_d0
 bl pop_to_d0
 ldr r0, =results
-add r0, r0, #8
+add r0, r0, #72
 vstr d0, [r0]
 
 
